9b0b85c11557a4d3828821b2ff6451964cfd831f
anlogan <anlogan@mines.edu>
2020-05-20 15:26:41 -0600

    Initial commit

---
+++ LCD_Show.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+
+#include "LCD.h"
+#include "DEV_Config.h"
+
+void setup()
+{
+  //pinMode(LCD_CS, 10);
+  //pinMode(LCD_RST, 29);
+  //pinMode(LCD_DC, 9);
+  pinMode(LCD_CS, OUTPUT);
+  pinMode(LCD_RST, OUTPUT);
+  pinMode(LCD_DC, OUTPUT);
+    //pinMode(LCD_DC, OUTPUT);
+  Serial.begin(115200);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+  SPI.begin();
+
+  LCD_SCAN_DIR Lcd_ScanDir = SCAN_DIR_DFT;
+  Serial.println("LCD Init ");
+  LCD.LCD_Init(Lcd_ScanDir);
+
+  Serial.println("LCD_Show ");
+  LCD.LCD_Show();
+}
+
+void loop()
+{
+
+
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
